ff2d9de25576497c81586f1ebcbb1cefb1ab555a
Gyuhyeon_Lee <mark3236@gmail.com>
2016-07-16 11:28:30 +0900

    Basic website design, now all routed correctly. ('/', '/demo', '/demo/submitpref')

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
+var index = require('./routes/index');
+var demo = require('./routes/demo');
 var submitpref = require('./routes/submitpref');
 
 var app = express();
 
-var requestTime = function (req, res, next){
-	req.testTime = new Date();
-	next();
-}
-app.use(requestTime);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,11 +23,126 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/submitpref', submitpref);
+//website routing
+app.use('/', index);
+app.use('/demo', demo);
+
+//requests
+//app.use('/submitpref', submitpref);
 
 
 
 
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-app.listen(8000);
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+//var port = normalizePort(process.env.PORT || '3000');
+var port = 8000;
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
--- routes/demo.js
+++ routes/demo.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var mysql = require('mysql');
 var router = express.Router();
+
 var mysqlConfig = {
 	'host' : 'slowbookdb.c0tygauz7qlt.ap-northeast-2.rds.amazonaws.com:3306',
 	'user' : 'slowbook', 'password' : 'godwjd1212', 'database' : 'slowbookdb',
 }
 var connection = mysql.createConnection(mysqlConfig);
 
-router.post('/', function(req, res, next){
+/* GET demo page */
+router.get('/', function(req, res, next) {
+  res.render('demo', { testtitle:'느림과 여유' });
+});
+
+//submitpref POST
+router.post('/submitpref', function(req, res, next){
 	//res.write('User gender: '+req.body.gender+'\n'+'User age: '+req.body.age+'\n'+'Selected genre: '+req.body.bookgenre);
-	res.redirect('')
+	res.redirect('http://xkcd.com')
 	res.end();
 });
 
