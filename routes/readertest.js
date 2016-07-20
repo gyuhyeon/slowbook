dbfd72269e7429e94e6b30b457bf00c140360e0c
Gyuhyeon_Lee <mark3236@gmail.com>
2016-07-21 02:49:15 +0900

    scraped retarded POST method, now correctly implemented with GET method properly, with option selected intact

--- routes/readertest.js
+++ routes/readertest.js
@@ -11,12 +11,19 @@ var connection = mysql.createConnection(mysqlConfig);
 /* GET reader test page */
 router.get('/', function(req, res, next) {
 	//var list='';
-
 	connection.query('SELECT * FROM Booklist',
 		function(error, cursor){
 			var list='';
+			var selectedbookurl='';
 			if(error==null){
 				for(var i=0; i<cursor.length; i++){
+					//search for book with selected book('booklist') p_id
+					if(cursor[i].p_id==req.query['booklist']){
+						selectedbookurl=cursor[i].bookurl;
+						list+='<option selected value="'+cursor[i].p_id+'">'+cursor[i].bookname+'</option>';
+						continue;
+					}
+					//add book to list with p_id value and bookname
 					list+='<option value="'+cursor[i].p_id+'">'+cursor[i].bookname+'</option>';
 				}
 			}
@@ -24,25 +31,44 @@ router.get('/', function(req, res, next) {
 				console.log(error);
 				list+='<option value="'+0+'">'+"Sorry, our servers aren't reachable at the moment.</option>";
 			}
-			//if there's any selected book, display as pdf.
-			if(typeof req.cookies.selectedbook == 'undefined'){
-				res.render('readertest', { testtitle:'느림과 여유', booklist:list, bookurl:''});
-			}
-			else{
-				res.render('readertest', { testtitle:'느림과 여유', booklist:list, bookurl:req.cookies.selectedbook});
-			}
-			console.log(req.cookies.selectedbook);
+			res.render('readertest', { testtitle:'느림과 여유', booklist:list, bookurl:selectedbookurl});
 		});
 		//can't render outside, because variable list is wiped.
 });
 
+/* Reason this didn't work : mysql is asynchronous.
+It doesn't make sense to put return value in a callback function inside a function. It was retarded of me.
+function queryurl(booklist){
+	connection.query('SELECT bookurl FROM Booklist WHERE p_id=?',booklist,
+		function(error, cursor){
+			if(error==null){
+				if(cursor.length>0){
+					console.log('1'+cursor[0].bookurl);
+					return cursor[0].bookurl;
+				}
+				else{
+					console.log('no cursor');
+					return '';
+				}
+			}
+			else{
+				console.log('db error');
+				return '';
+			}
+		});
+}
+*/
+
+/*
 //submitpref POST
-router.post('/submitpref', function(req, res, next){
+//it was stupid. Finally fixed now.
+router.get('/submitpref', function(req, res, next){
 	connection.query('SELECT bookurl FROM Booklist WHERE p_id=?',[req.body.booklist],
 		function(error, cursor){
 			if(error==null){
 				if(cursor.length>0){
 					//set cookie. Is this really the only way to 'refresh' the page while retaining information???!
+					//------------NEED TO CHANGE THIS------------------
 					res.cookie('selectedbook', cursor[0].bookurl, {maxAge: 60*1000*24});
 					res.redirect('/readertest');
 				}
@@ -57,5 +83,5 @@ router.post('/submitpref', function(req, res, next){
 			}
 		});
 });
-
+*/
 module.exports = router;
