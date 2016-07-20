5fa89945616a900710811750fd49ebf1f79ce5ec
Gyuhyeon_Lee <mark3236@gmail.com>
2016-07-21 05:02:36 +0900

    routing issues fixed(was missing / in front of ejs templates), secret catgirl stash more secure.

--- routes/ahri.js
+++ routes/ahri.js
@@ -4,6 +4,10 @@ var router = express.Router();
 
 /* GET reader test page */
 router.get('/', function(req, res, next) {
+	res.render('ahri-confirm');
+});
+
+router.get('/easteregg', function(req, res, next) {
 	res.render('ahri', { testtitle:'아리는 사랑입니다'});
 });
 
